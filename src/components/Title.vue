109c158ea4a978d3462117041e1cac00eace3e57
AlessioDPT <alessio.recos@gmail.com>
2021-07-05 16:42:36 +0200

    ADD- componente title

---
+++ src/components/Title.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+
+    <h1>La mia prima app con Vue CLI!</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+    font-size: 40px;
+    color: darkred;
+}
+
+</style>
